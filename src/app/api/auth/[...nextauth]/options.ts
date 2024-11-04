424629e95c26df8df366914f2de9e21c80fb807e
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2024-11-04 14:05:45 +0100

    access token

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -8,9 +8,22 @@ export const options: AuthOptions = {
 			clientSecret: process.env.GITHUB_SECRET!,
 			authorization: {
 				params: {
-					scope: 'repo_repo',
+					scope: 'public_repo', // Adjusted scope
 				},
 			},
 		}),
 	],
+	callbacks: {
+		async jwt({ token, account }) {
+			if (account) {
+				token.accessToken = account.access_token;
+			}
+			return token;
+		},
+		async session({ session, token }) {
+			// Cast token.accessToken to string | undefined
+			session.accessToken = token.accessToken as string | undefined;
+			return session;
+		},
+	},
 };
